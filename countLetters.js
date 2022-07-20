9f2106d74f7e0308f9f3ffbf30030dba75b16a05
m-khalaf <mkhalaf2211@gmail.com>
2022-07-20 16:19:23 +0000

    created a function to count letters in string

---
+++ countLetters.js
@@ -0,0 +1,16 @@
+
+
+const countLetters =function(strgs){
+  let strs = strgs.split(' ').join("")
+  let result ={};
+  for (let str of strs){
+    if (result[str]){
+      result[str]+=1;
+    }
+    else
+    result[str]=1;
+  }
+  return result
+}
+
+console.log(countLetters("lighthouse in the house"))
