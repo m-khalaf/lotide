b7144f8bb8a57e588a8bd4bb127e56a9db69d80f
m-khalaf <mkhalaf2211@gmail.com>
2022-08-02 22:37:12 +0000

    refactored the function and added a test file

--- eqArrays.js
+++ eqArrays.js
@@ -7,8 +7,14 @@ const assertEqual = function(actual, expected) {
   }
 };
 const eqArrays = function(arr1,arr2) {
-  let equal;
+  let equal=true;
+  if (arr1.length !== arr2.length) equal= false;
+
   for (let i = 0; i < arr1.length; i++) {
+    
+    if(Array.isArray(arr1[i]) && Array.isArray(arr2[i])){
+       if(!eqArrays(arr1[i],arr2[i])) equal = false;
+    }
     if (arr1[i] === arr2[i]) {
       equal = true;
     } else equal = false;
@@ -20,4 +26,8 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false); // => false
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]),true) // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]),false) // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]),false) // => false
 
--- head.js
+++ head.js
@@ -1,18 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertation Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertation Failed: ${actual} !== ${expected}`);
-  
-  }
-};
-
 const head = function(arr) {
   return arr[0];
-  // if (arr===[])
-  // return undefined;
+  
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]),5);
+module.exports = head;
+
