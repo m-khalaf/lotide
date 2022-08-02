47fb6981e9e7a369aabe7e05a931897417ded152
m-khalaf <mkhalaf2211@gmail.com>
2022-08-02 22:35:10 +0000

    refactored the function and added a test file

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual(2,4);
-assertEqual(2,'bootcamp');
-assertEqual('bootcamp','bootcamp');
-assertEqual(2,2);
 
+
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual(2,4);
+assertEqual(2,'bootcamp');
+assertEqual('bootcamp','bootcamp');
+assertEqual(2,2);
