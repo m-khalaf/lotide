925c7ae9f216b9115bfb692f0ad3444684f3d363
m-khalaf <mkhalaf2211@gmail.com>
2022-08-03 02:34:58 +0000

    used mocha to test the function

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,26 @@
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe('#tail', ()=>{
+  it('should return [2,3]',()=>{
+    assert.deepEqual(tail([1,2,3]),[2,3]);
+  });
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  it('should return [2,"3"]',()=>{
+    assert.deepEqual(tail([1,2,'3']),[2,'3']);
+  });
+})
 
-const emptyArray = [];
-console.log(tail(emptyArray));
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// const emptyArray = [];
+// console.log(tail(emptyArray));
