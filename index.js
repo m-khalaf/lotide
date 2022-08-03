d0fed7e91be7d3cd7fdd17f0b4726ecb43f6d63a
m-khalaf <mkhalaf2211@gmail.com>
2022-08-03 03:33:06 +0000

    required all functions in one index file

---
+++ index.js
@@ -0,0 +1,14 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArrayEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertArrayEqual: assertArrayEqual,
+  assertEqual: assertEqual
+}
--- middle.js
+++ middle.js
@@ -13,9 +13,4 @@ const middle = function (arr) {
     return midArray
 }
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports =middle;
