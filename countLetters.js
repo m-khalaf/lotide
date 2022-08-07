ac0cae3c4c6bbdb842faee6b559f47d4c651efdd
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 14:04:12 +0000

    added comments, added {} to code blocks, and added export line

--- countLetters.js
+++ countLetters.js
@@ -1,16 +1,14 @@
-
-
-const countLetters =function(strgs){
-  let strs = strgs.split(' ').join("")
-  let result ={};
-  for (let str of strs){
-    if (result[str]){
-      result[str]+=1;
+const countLetters = function(strgs) {
+  let strs = strgs.split(' ').join(""); //removes all space between the letters and joins them in a single string
+  let result = {};
+  for (const str of strs) {
+    if (result[str]) { //checks if the key already exists in the object and if so it increments by one
+      result[str] += 1;
+    } else {
+      result[str] = 1; //the key is not in the objects so it creates a new key and assigns value of 1
     }
-    else
-    result[str]=1;
   }
   return result;
-}
+};
 
-console.log(countLetters("lighthouse in the house"))
+module.exports = countLetters;
