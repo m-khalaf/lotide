b3c649619271ac766b432f6dcc7e27faaa706590
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 14:05:18 +0000

    added comments and added {} to code blocks

--- countOnly.js
+++ countOnly.js
@@ -1,16 +1,16 @@
-const countOnly = function(allItems, itemsToCount){
+const countOnly = function(allItems, itemsToCount) {
 
-  const results ={};
-  for (let item of allItems){
-    if (itemsToCount[item]){
-      if (results[item])
-      results[item]+=1;
-      else 
-      results[item]=1;
+  const results = {};
+  for (const item of allItems) {//loops over the original array and checks if each item exists in the itemsToCOunt array
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1; //checks if the key exists in the object and if yes it increments by 1
+      } else {
+        results[item] = 1; //the key is not in the objects so it creates a new key and assigns value of 1
+      }
     }
-    
   }
   return results;
-}
+};
 
-module.exports= countOnly;
+module.exports = countOnly;
