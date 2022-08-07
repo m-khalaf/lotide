5624344f496e864ed759630b8733f1a5c76d317e
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 14:08:02 +0000

    removed redefined functions, added comments and {} to code blocks

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,14 @@
-const eqArrays = function (arr1, arr2) {
-  //let equal=true;
-  if (arr1.length !== arr2.length) return false;
-
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
-
-    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
-      //console.log('both are arrays');
-       if(!eqArrays(arr1[i],arr2[i])) return false;
-    }else if (arr1[i] !== arr2[i]) {
-      return false
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) { //checks if both elements are arrays and if yes it uses recursion to call back the same function
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) { //if items are not arrays it compares them as values
+      return false;
     }
   }
   return true;
