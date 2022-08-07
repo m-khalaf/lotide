79d937fbe1b54674976a96adea0259a281512f7a
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 14:17:20 +0000

    added comments

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,9 @@
-const findKeyByValue = function(obj, value){
-  for (let key in obj){
-    if(obj[key]===value)
-    return key
+const findKeyByValue = function(obj, value) {
+  for (const key in obj) {
+    if (obj[key] === value) { //loops over the object and checks if the value passed to the function matches the value in the objects and if so it returns the key
+      return key;
+    }
   }
-}
+};
 
-module.exports=findKeyByValue;
-
-
+module.exports = findKeyByValue;
