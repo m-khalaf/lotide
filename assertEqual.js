9d34a25dbc34e6ae2ed0d1e5279661ae8562829d
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 14:00:53 +0000

    added {} to code blocks, and added return true and false values

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertation Passed: ${actual} === ${expected}`);
+    return true;
   } else {
     console.log(`❌❌❌Assertation Failed: ${actual} !== ${expected}`);
-
+    return false;
   }
 };
-
-
 
 module.exports = assertEqual;
