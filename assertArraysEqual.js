5913e37cc3de9d24310432eda819004d1fd2e4c1
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 13:59:24 +0000

    added comments and removed dead functions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,13 @@
 const eqArrays = require('./eqArrays');
 
-  const assertArraysEqual =function(ar1,ar2){
-    if(eqArrays(ar1,ar2))
-    console.log('arrays are equal')
-    else console.log('arrays are not equal')
+const assertArraysEqual = function(ar1, ar2) {
+  if (eqArrays(ar1, ar2)) {  //calls eqArrays function to check if arrays are equal
+    console.log('arrays are equal');
+    return true;
+  } else {
+    console.log('arrays are not equal');
+    return false;
   }
+};
 
-  module.exports = assertArraysEqual;
+module.exports = assertArraysEqual;
