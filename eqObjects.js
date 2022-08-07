fe00e1803590f68482f315ed6bf41c66b5c3f36b
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 14:12:52 +0000

    removed redefined functions, added comments, and fixed code blocks

--- eqObjects.js
+++ eqObjects.js
@@ -1,48 +1,25 @@
 const eqArrays = require('./eqArrays');
-const assertEqual = require('./assertEqual');
 
-const eqObjects = function (object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) return false;
-  for (let obj in object1) {
-    if (Array.isArray(object1[obj]) && Array.isArray(object2[obj])) {
-      if (!eqArrays(object1[obj], object2[obj])) return false;
-
-    } else if (typeof (object1[obj]) === 'object' && typeof (object2[obj]) === 'object') {
-       if(!eqObjects(object1[obj],object2[obj])) return false;
-    }
-
-    else {
-      if (object1[obj] !== object2[obj]) {
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) { //checks if objects are equal in length and if not returns false 
+    return false;
+  }
+  for (const obj in object1) {
+    if (Array.isArray(object1[obj]) && Array.isArray(object2[obj])) { //checks if both key-pair values in the objects are arrays and if so it calls the eqArray function
+      if (!eqArrays(object1[obj], object2[obj])) { 
+        return false;
+      }
+    } else if (typeof (object1[obj]) === 'object' && typeof (object2[obj]) === 'object') { //checks if both key-pair values in the objects are objects and if so it uses recursion to call eqObjects function
+      if (!eqObjects(object1[obj], object2[obj])) {
+        return false;
+      }
+    } else {
+      if (object1[obj] !== object2[obj]) { //if key-pair values are not arrays or objects then it compares them as premitive values
         return false;
       }
     }
-
   }
   return true;
 };
 
-module.exports =eqObjects;
-
-console.log("Primitives As Values")
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
-
-console.log("\nArrays As Values")
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
-
-console.log("\nObjects As Values")
-
-console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
-console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
-console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
-console.log(eqObjects({ a: { y: {c:2}, z: 1 }, b: 2 }, { a: { y:{c:2}, z: 1 }, b: 2 }))//=>true
+module.exports = eqObjects;
