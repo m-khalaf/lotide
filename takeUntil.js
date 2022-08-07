eea1e8cd9832b9a970afa76a56407210b43bb367
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 14:34:08 +0000

    added export line, comments and code blocks

--- takeUntil.js
+++ takeUntil.js
@@ -1,33 +1,16 @@
-const takeUntil = function (array, callback) {
+const takeUntil = function(array, callback) {
   let arr = array;
-  newArray = [];
+  let newArray = [];
   console.log(arr);
   let i = 0;
-  while (!callback(arr[i])) {
-    if (callback(arr[i])) break;
-    else newArray.push(arr.shift());
+  while (!callback(arr[i])) {// while loop that keeps running as long as the callback function is returning false
+    if (callback(arr[i])) { //if the callback functions returns true it breaks from the loop
+      break;
+    } else {
+      newArray.push(arr.shift());//pushes the values that did not pass the callback to a result array
+    }
   }
   return newArray;
+};
 
-  /* for (let i = 0; i < arr.length; i++) {
-    // console.log(arr[i])
-    // console.log(callback(arr[i]));
-    // console.log(i)
-    if (callback(arr[i])) return newArray;
-    else newArray.push(arr.shift());
-
-  }
-  return newArray; */
-
-}
-
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+module.exports = takeUntil;
