349ad80ec250b673554e887b9d3a369cbea3df96
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 14:23:12 +0000

    added comments and code blocks {}

--- flatten.js
+++ flatten.js
@@ -1,14 +1,14 @@
-const flatten = function (arr) {
+const flatten = function(arr) {
   let result = [];
-  for (let x of arr) {
-    if (Array.isArray(x)) {
+  for (const x of arr) {
+    if (Array.isArray(x)) { //checks if element is an array and if so uses recursion to call back the same function and concats the result to the final result
       result = result.concat(flatten(x));
+    } else {
+      result.push(x); //if the element is just a premitive value then it just pushes it to the result array
     }
-    else result.push(x);
   }
-
   return result;
-}
+};
 
 module.exports = flatten;
 
