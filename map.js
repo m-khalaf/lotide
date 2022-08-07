d167c704a9a4266033cbada6d561e0c2cacc7308
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 14:26:36 +0000

    added comments and export line

--- map.js
+++ map.js
@@ -1,10 +1,8 @@
-const words = ["ground", "control", "to", "major", "tom"];
-const map = function(array,callback){
-  const results=[];
-  for (let item of array){
+const map = function(array,callback) {
+  const results = [];
+  for (const item of array) { //loops over the array and calls the calls the callback function and pushes the result of the callback into the results array
     results.push(callback(item));
   }
-return results;
+  return results;
 };
-const results1 = map(words, word => word[0]);
-console.log(results1);
+module.exports = map;
