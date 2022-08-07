eb3a5eb86459d1cf209e53d22e889c6b63d54082
m-khalaf <mkhalaf2211@gmail.com>
2022-08-07 14:14:34 +0000

    exported the function, and added comments

--- findKey.js
+++ findKey.js
@@ -1,19 +1,9 @@
-const findKey = function(objs, callback){
-  for (let key in objs ){
-    if (callback(objs[key]))
-    return key
+const findKey = function(objs, callback) {
+  for (const key in objs) {
+    if (callback(objs[key])) { // passes every key value to the call back function and checks if it passes and returns the key
+      return key;
+    }
   }
-}
+};
 
-
-
-
-
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)) // => "noma"
+module.exports = findKey;
